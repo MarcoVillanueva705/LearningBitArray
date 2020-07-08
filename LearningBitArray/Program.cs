4dbbea5e160f41438cea82c149354c7fe1a7f0b7
marco villanueva <villanuevamarco98@gmail.com>
2020-07-07 20:55:48 -0600

    Add project files.

---
+++ LearningBitArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace LearningBitArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //pass the array constructor to the BitArray
+
+            bool[] preload = new bool[3] { true, false, true };
+
+            //BitArray will need to know size initialize with, so pass (3)
+            //to the constructor
+            //BitArray enemyGrid = new BitArray(3);
+
+            BitArray enemyGrid = new BitArray(preload);
+
+            foreach (var item in enemyGrid)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
